973e03afb8454112e2c86b8ee95ec20b965dc123
Johan Kremer <johan.h.kremer@gmail.com>
2026-06-01 11:49:26 +0200

    add examples

--- script/services/weatherService.js
+++ script/services/weatherService.js
@@ -1,6 +1,6 @@
 //! Använd inte Johans nyckel! Generera din egen.
 const BASE_URL = "https://api.openweathermap.org"
-const OPEN_WEATHER_KEY = "a684851d73c32ad6b7a91b52c30311c1"
+const OPEN_WEATHER_KEY = null
 
 export const getWeatherCurrent = async (lat, lon) => {
     const response = await fetch(`${BASE_URL}/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_KEY}&units=metric&lang=sv`)
