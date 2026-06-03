19bb754f054a20308cb5c8f303e4594e59ecf2ab
Johan Kremer <johan.h.kremer@gmail.com>
2026-06-03 09:12:10 +0200

    add examples

--- script/services/weatherService.js
+++ script/services/weatherService.js
@@ -1,6 +1,6 @@
 //! Använd inte Johans nyckel! Generera din egen.
 const BASE_URL = "https://api.openweathermap.org"
-const OPEN_WEATHER_KEY = null
+const OPEN_WEATHER_KEY = "082b871a5bb53d73e79b7a95faeb958f"
 
 // weatherService.js är en "service-modul": den ansvarar för API-anrop.
 // Andra filer kan importera funktionerna, men behöver inte känna till URL:erna.
